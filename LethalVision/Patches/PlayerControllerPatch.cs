10c53a24fe824dd4bc4ed76f9499603a5e787c8d
legoandmars <legoandmars@gmail.com>
2023-12-26 22:19:08 -0800

    improve rainbow ui:

--- LethalVision/Patches/PlayerControllerPatch.cs
+++ LethalVision/Patches/PlayerControllerPatch.cs
@@ -38,25 +38,28 @@ namespace LethalVision.Patches
                 ToggleCustomPass(additionalCameraData, true);
             }
 
-            TextMeshProUGUI[] texts = Resources.FindObjectsOfTypeAll(typeof(TextMeshProUGUI)) as TextMeshProUGUI[];
-            var materials = texts.Select(x => x.fontSharedMaterial).Distinct();
-            foreach(var material in materials)
-            {
-                if (material.shader.name == "TextMeshPro/Distance Field")
-                {
-                    material.shader = Plugin.RainbowTextShader;
-                    Debug.Log("Replacing.");
-                }
-            }
+            Rainbowify();
         }
 
-        private List<string> _whiteListedSpritesRainbowUI = new()
+        private static List<string> _whiteListedSpritesRainbowUI = new()
         {
-            "DialogueBox1Frame 2",
-            "SprintMeter"
+            "DialogueBox1Frame 2", // used for chat menu, inventory slots, etc
+            "SprintMeter",
+            "scanCircle1",
+            "scanCircle2",
+            "ScanUI1",
+            "DialogueBoxSimple"
         };
 
-        private void Rainbowify()
+        private static List<string> _whiteListedNamesRainbowUI = new()
+        {
+            "HeaderTextBox", // scan node name
+            "SubTextBox", // scan node price background
+            "Panel (2)", // pause menu background
+            "Panel (1)" // pause menu outer decoration
+        };
+
+        private static void Rainbowify()
         {
             TextMeshProUGUI[] texts = Resources.FindObjectsOfTypeAll(typeof(TextMeshProUGUI)) as TextMeshProUGUI[];
             var materials = texts.Select(x => x.fontSharedMaterial).Distinct();
@@ -72,10 +75,19 @@ namespace LethalVision.Patches
             Image[] images = Resources.FindObjectsOfTypeAll(typeof(Image)) as Image[];
             foreach (var image in images)
             {
-                if(image.sprite?.name != null && _whiteListedSpritesRainbowUI.Any(x => x == image.sprite.name))
+                // TODO: If there's no sprite, we can just use the vertex color to save a ton of performance
+                // Come up with second vertex-only shader
+                if((image.sprite?.name != null && _whiteListedSpritesRainbowUI.Any(x => x == image.sprite.name)) || _whiteListedNamesRainbowUI.Any(x => x == image.gameObject.name))
                 {
                     // instance material
                     // TODO Fancier fix
+                    image.material = UnityEngine.Object.Instantiate(image.material);
+                    image.material.shader = Plugin.RainbowUIShader;
+                }
+                else if(image.gameObject.name == "Panel" && image.transform.parent.name == "PleaseConfirmChangesPanel")
+                {
+                    // specifically for the generically-named panel when confirming settings
+                    image.material = UnityEngine.Object.Instantiate(image.material);
                     image.material.shader = Plugin.RainbowUIShader;
                 }
             }
