ffd9b6ac0b950ae5c5b958ee8c8bf9bf099fe7fa
legoandmars <legoandmars@gmail.com>
2023-12-30 12:27:14 -0800

    Image shader replacement by sprite name

---
+++ LethalVision/Models/ImageNameShaderReplacement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LethalVision.Models
+{
+    internal class ImageNameShaderReplacement : ImageShaderReplacement
+    {
+        private Shader _replacement;
+        private string _spriteName;
+
+        public ImageNameShaderReplacement(Shader replacement, string spriteName)
+        {
+            _spriteName = spriteName;
+            _replacement = replacement;
+        }
+
+        public override bool ImageShaderIsMatch(Image image)
+        {
+            // "TextMeshPro/Distance Field"
+            return image.sprite.name == _spriteName;
+        }
+
+        public override void ReplaceImageShader(Image image)
+        {
+            if (_originalMaterialsByImage.ContainsKey(image)) return;
+
+            _originalMaterialsByImage.Add(image, image.material);
+
+            image.material = UnityEngine.Object.Instantiate(image.material);
+            image.material.shader = _replacement;
+        }
+
+        public override void RestoreImageShaders()
+        {
+            foreach (var (image, material) in _originalMaterialsByImage)
+            {
+                image.material = material;
+            }
+
+            _originalMaterialsByImage.Clear();
+        }
+
+        public override string ToString()
+        {
+            return $"ImageShaderReplacement (Name): {_replacement.name} (replacing sprite: {_spriteName})";
+        }
+    }
+}
---
+++ LethalVision/Models/ImageShaderReplacement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LethalVision.Models
+{
+    // used to keep track of changes we make to the game so we can undo them
+    // meant to instantiate material for now, maybe eventually it could have some high-effort deduplication
+    internal abstract class ImageShaderReplacement
+    {
+        // https://www.youtube.com/watch?v=x0H_WPzoTb0
+        protected Dictionary<Image, Material> _originalMaterialsByImage = new();
+
+        public bool ImageShadersReplaced => _originalMaterialsByImage.Count > 0;
+
+        public void ReplaceImageShaderIfMatch(Image image)
+        {
+            if (!ImageShaderIsMatch(image)) return;
+            ReplaceImageShader(image);
+        }
+
+        public abstract bool ImageShaderIsMatch(Image image);
+        public abstract void ReplaceImageShader(Image image);
+        public abstract void RestoreImageShaders();
+    }
+}
