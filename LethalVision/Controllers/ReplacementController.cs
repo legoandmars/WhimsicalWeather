47aa4ba4884c77806a93a2718cb79c6a3fba31d5
legoandmars <legoandmars@gmail.com>
2023-12-30 13:21:14 -0800

    hook up image replacement

--- LethalVision/Controllers/ReplacementController.cs
+++ LethalVision/Controllers/ReplacementController.cs
@@ -9,19 +9,61 @@ namespace LethalVision.Controllers
 {
     internal class ReplacementController
     {
-        private List<MaterialReplacement> materialReplacements = new();
+        private static List<string> _imageSpriteNames = new()
+        {
+            "DialogueBox1Frame 2", // used for chat menu, inventory slots, etc
+            "SprintMeter",
+            "scanCircle1",
+            "scanCircle2",
+            "ScanUI1",
+            "DialogueBoxSimple",
+            "BatteryIcon",
+            "BatteryMeter",
+            "BatteryMeterFrame"
+        };
+
+        // use "/" to denote parent gameObject
+        private static List<string> _imageGameObjectNames = new()
+        {
+            "HeaderTextBox", // scan node name
+            "SubTextBox", // scan node price background
+            "Panel (2)", // pause menu background
+            "Panel (1)", // pause menu outer decoration
+            "PleaseConfirmChangesPanel/Panel"
+        };
+
+        private List<MaterialReplacement> _materialReplacements = new();
+        private List<ImageShaderReplacement> _imageShaderReplacements = new();
 
         public void CreateTextureReplacements(List<TextureReplacementBehaviour> textureReplacements)
         {
             foreach (var replacement in textureReplacements)
             {
-                materialReplacements.Add(new TextureReplacement(replacement));
+                _materialReplacements.Add(new TextureReplacement(replacement));
+            }
+        }
+
+        public void CreateImageShaderReplacements()
+        {
+            foreach (var imageSpriteName in _imageSpriteNames)
+            {
+                var replacement = new ImageNameShaderReplacement(Plugin.RainbowUIShader, imageSpriteName);
+                _imageShaderReplacements.Add(replacement);
+            }
+            foreach (var imageGameObjectName in _imageGameObjectNames)
+            {
+                ImageGameObjectNameShaderReplacement replacement;
+                var split = imageGameObjectName.Split('/');
+                if (split.Length > 1) replacement = new(Plugin.RainbowUIShader, split[1], split[0]);
+                else replacement = new(Plugin.RainbowUIShader, imageGameObjectName);
+
+                _imageShaderReplacements.Add(replacement);
             }
         }
 
         private void ReplaceTextures(List<Material> materials)
         {
-            foreach (var replacementInfo in materialReplacements)
+            foreach (var replacementInfo in _materialReplacements)
             {
                 foreach (var material in materials)
                 {
